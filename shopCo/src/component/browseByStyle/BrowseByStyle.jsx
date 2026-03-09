10b9119688f8c492661716e344bc837472e0fe90
OlaAdams <adamsonoladipupo>
2026-03-09 06:10:28 +0100

    new updae

--- shopCo/src/component/browseByStyle/BrowseByStyle.jsx
+++ shopCo/src/component/browseByStyle/BrowseByStyle.jsx
@@ -1,6 +1,11 @@
+import styles from '../browseByStyle/browseByStyle.module.css'
+
 const BrowseByStyle = () => {
     return (
         <>
+        <div className={styles.main_container}>
+
+        </div>
         </>
     );
 }
