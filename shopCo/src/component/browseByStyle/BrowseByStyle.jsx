848b10c53f67638fbaf24f702fb430c3552264c2
OlaAdams <adamsonoladipupo>
2026-03-09 06:27:58 +0100

    creating html of browse by style component

--- shopCo/src/component/browseByStyle/BrowseByStyle.jsx
+++ shopCo/src/component/browseByStyle/BrowseByStyle.jsx
@@ -1,10 +1,25 @@
 import styles from '../browseByStyle/browseByStyle.module.css'
+import images from '../../assets/images';
 
 const BrowseByStyle = () => {
     return (
         <>
         <div className={styles.main_container}>
-            
+            <h2>BROWSE BY dress STYLE</h2>
+            <div className={styles.clothe_styles}>
+                <span className="style">
+                    <img src={images.casualStyle} alt="casual" />
+                </span>
+                <span className="style">
+                    <img src={images.formalStyle} alt="formal" />
+                </span>
+                <span className="style">
+                    <img src={images.partyStyle} alt="party" />
+                </span>
+                <span className="style">
+                    <img src={images.gymStyle} alt="gym" />
+                </span>
+            </div>
         </div>
         </>
     );
