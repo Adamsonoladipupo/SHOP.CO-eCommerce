cbd43a5ed36e70a5cf92cd7e1ea5e70630bf6283
OlaAdams <adamsonoladipupo>
2026-03-10 17:07:52 +0100

    new commit

--- shopCo/src/component/newArrivals/NewArrivals.jsx
+++ shopCo/src/component/newArrivals/NewArrivals.jsx
@@ -2,40 +2,33 @@ import styles from "../newArrivals/newArrivals.module.css"
 import images from "../../assets/images";
 import {Star } from '../../assets/icons'
 import { useGetAllProductsQuery } from "../../api/ProductAPI";
+import { useState } from "react";
 
 
 const NewArrivals = () => {
     const data = useGetAllProductsQuery()
-    console.log(data)
+    const [expand, setExpand] = useState(false);
+    const changeView = () => setExpand(showed => !showed)
+    console.log(changeView)
 
     return(
         <>
             <div className={styles.main_container}>
                 <h2>NEW ARRIVALS</h2>
-                {/* <div className={styles.new_arrivals}>
-                    <div className={styles.arrival}>
-                        <span className={styles.clothe_image} style={{backgroundImage:`URL(${images.clothe1})`, backgroundSize:'cover', backgroundPosition:'center'}}></span>
-                        <h3>T-SHIRT WITH TAPE DETAILS</h3>
-                        <span><Star/><Star/><Star/><Star/>4.5</span>
-                        <span className={styles.price}>$120</span>
-                    </div>
-                </div> */}
-
                 <div className={styles.new_arrivals}>
                     {
                         data.data?.map((product) =>(
                             <div key={product.id} className={styles.arrival}>
                                 <span className={styles.clothe_image} style={{backgroundImage:`URL(${product.image})`, backgroundSize:'cover', backgroundPosition:'center'}}></span>
                                 <h3>{product.title}</h3>
-                                {/* <span><Star/><Star/><Star/><Star/>{product.rating.rate}</span> */}
+                                <span><Star/><Star/><Star/><Star/>{product.rating.rate}</span>
                                 <span className={styles.price}>${product.price}</span>
                             </div>
                         ))
                     }
                 </div>
 
-
-                <button>View All</button>
+                <button onClick={changeView}>View All</button>
             </div>
         </>
     );
