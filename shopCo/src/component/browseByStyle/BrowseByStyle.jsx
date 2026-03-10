6f5fb5f706cc6ef1aed93ceab6c8986f3bca888a
OlaAdams <adamsonoladipupo>
2026-03-10 12:49:42 +0100

    new commit

--- shopCo/src/component/browseByStyle/BrowseByStyle.jsx
+++ shopCo/src/component/browseByStyle/BrowseByStyle.jsx
@@ -5,7 +5,7 @@ const BrowseByStyle = () => {
     return (
         <>
         <div className={styles.main_container}>
-            <h2>BROWSE BY dress STYLE</h2>
+            <h2>BROWSE BY DRESS STYLE</h2>
             <div className={styles.clothe_styles}>
                 <div className={`${styles.style} ${styles.small_width}`} style={{backgroundImage:`url(${images.casualStyle})`}}>
                     <span>Casual</span>
@@ -13,10 +13,10 @@ const BrowseByStyle = () => {
                 <div className={`${styles.style} ${styles.large_width}`} style={{backgroundImage:`url(${images.formalStyle})`}}>
                     <span>Formal</span>
                 </div>
-                <div classNam={`${styles.style} ${styles.small_width}`} style={{backgroundImage:`url(${images.partyStyle})`}}>
+                <div className={`${styles.style} ${styles.small_width}`} style={{backgroundImage:`url(${images.partyStyle})`, backgroundPosition:'center'}}>
                     <span>Party</span>
                 </div>
-                <div className={`${styles.style} ${styles.large_width}`} style={{backgroundImage:`url(${images.gymStyle})`}}>
+                <div className={`${styles.style} ${styles.large_width}`} style={{backgroundImage:`url(${images.gymStyle})`, backgroundPosition:'center'}}>
                     <span>Gym</span>
                 </div>
             </div>
--- shopCo/src/component/newArrivals/NewArrivals.jsx
+++ shopCo/src/component/newArrivals/NewArrivals.jsx
@@ -1,21 +1,40 @@
 import styles from "../newArrivals/newArrivals.module.css"
 import images from "../../assets/images";
 import {Star } from '../../assets/icons'
+import { useGetAllProductsQuery } from "../../api/ProductAPI";
 
 
 const NewArrivals = () => {
+    const data = useGetAllProductsQuery()
+    console.log(data)
+
     return(
         <>
             <div className={styles.main_container}>
                 <h2>NEW ARRIVALS</h2>
-                <div className={styles.new_arrivals}>
+                {/* <div className={styles.new_arrivals}>
                     <div className={styles.arrival}>
                         <span className={styles.clothe_image} style={{backgroundImage:`URL(${images.clothe1})`, backgroundSize:'cover', backgroundPosition:'center'}}></span>
                         <h3>T-SHIRT WITH TAPE DETAILS</h3>
                         <span><Star/><Star/><Star/><Star/>4.5</span>
                         <span className={styles.price}>$120</span>
                     </div>
+                </div> */}
+
+                <div className={styles.new_arrivals}>
+                    {
+                        data.data?.map((product) =>(
+                            <div key={product.id} className={styles.arrival}>
+                                <span className={styles.clothe_image} style={{backgroundImage:`URL(${product.image})`, backgroundSize:'cover', backgroundPosition:'center'}}></span>
+                                <h3>{product.title}</h3>
+                                {/* <span><Star/><Star/><Star/><Star/>{product.rating.rate}</span> */}
+                                <span className={styles.price}>${product.price}</span>
+                            </div>
+                        ))
+                    }
                 </div>
+
+
                 <button>View All</button>
             </div>
         </>
